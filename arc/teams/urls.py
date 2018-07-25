3a84de521238772ffe2394ac82503e373587074d
Neeraj <neerajrajpurohitnn@gmail.com>
2018-07-25 14:46:34 +0530

    commit.

--- arc/teams/urls.py
+++ arc/teams/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
-from .views import teams, user_register, checkusername, checkteamname
+from .views import teams, randomresponse, user_register, checkusername, checkteamname
 #from .views import user_register, checkusername, team_register, teams, example
 from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
 
     url(r'register/$', user_register.as_view(), name = 'register'),
-    url(r'team_register/$', teams.as_view() , name = 'tregister'),
+   url(r'team_register/$', teams.as_view() , name = 'tregister'),
     url(r'checkusername/$', checkusername, name = 'checkusername'),
-    url(r'showteams/$', teams.as_view(), name = 'showteams'),
+     url(r'showteams/$', teams.as_view() , name = 'showteams'),
     url(r'checkteamname/$', checkteamname, name = 'checkteamname'),
     url(r'showusers/$', user_register.as_view(), name = 'showusers')
 
--- arc/teams/views.py
+++ arc/teams/views.py
@@ -4,12 +4,18 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.http import JsonResponse
 from .serialisers import team_dataSerialiser, user_recordSerialiser
+#user_recordSerialiser
 from .models import user_record, team_data, team
 import json
 from django.http import JsonResponse, HttpResponse
 from django.contrib.auth import authenticate
 from django.views.decorators.csrf import csrf_exempt
 from .forms import user
+
+def randomresponse(request):
+    return HttpResponse('sdfsdfsdfs')
+
+
 
 
 
@@ -27,11 +33,9 @@ class user_register(APIView):
         return Response(request.data, status=status.HTTP_400_BAD_REQUEST)
 
 
-
-
-
 class teams(APIView):
     def get(self, request):
+        #return HttpResponse('sdfsdfsdfs')
         teams = team_data.objects.all()
         serializer = team_dataSerialiser(teams, many = True)
         return Response(serializer.data)
@@ -42,6 +46,8 @@ class teams(APIView):
            serializer.save()
            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(request.data, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 
 
@@ -68,7 +74,6 @@ def checkteamname(request):
         return HttpResponse(json.dumps({'presence': True}), content_type='application/json')
     else:
         return HttpResponse(json.dumps({'presence'  : False }), content_type='application/json')
-
 
 
 
