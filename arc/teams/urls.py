8c85bbf3ac8d87b8ad38fc0b553cb02ffa90605f
neeraj3029 <31539812+neeraj3029@users.noreply.github.com>
2018-07-26 11:19:04 +0530

    Update urls.py

--- arc/teams/urls.py
+++ arc/teams/urls.py
@@ -1,16 +1,14 @@
 from django.conf.urls import url
-from .views import teams, randomresponse, checkusername, checkteamname
-#from .views import user_register, checkusername, team_register, teams, example
+from .views import teams, checkusername, checkteamname
 from .views import user_register
 from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
-
-    url(r'register/$', user_register.as_view(), name = 'register'),
-    # url(r'team_register/$', teams.as_view() , name = 'tregister'),
-    url(r'checkusername/$', checkusername, name = 'checkusername'),
-    url(r'showteams/$', teams.as_view() , name = 'showteams'),
-    url(r'checkteamname/$', checkteamname, name = 'checkteamname'),
-    url(r'showusers/$', user_register.as_view(), name = 'showusers')
+    
+    url(r'register/$', user_register.as_view(), name = 'register'), #url for user registration
+    url(r'checkusername/$', checkusername, name = 'checkusername'), #checks that the game tag is available or not
+    url(r'showteams/$', teams.as_view() , name = 'showteams'), # on post request a team registered and and it all the on get req
+    url(r'checkteamname/$', checkteamname, name = 'checkteamname'), #checks that the team name is available or not
+    url(r'showusers/$', user_register.as_view(), name = 'showusers') #returns all the current users with their taga
 
 ]
 
